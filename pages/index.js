dd788548065b7076396217cb42dae78a137c18dd
Austin Wang <austinpower1258@gmail.com>
2022-01-17 12:35:33 -0800

    export supabase

--- pages/index.js
+++ pages/index.js
@@ -1,11 +1,93 @@
-import Head from 'next/head'
-import Image from 'next/image'
-import styles from '../styles/Home.module.css'
+import supabase from "../lib/supabase";
+import { useState, useEffect } from "react";
 
 export default function Home() {
+  const [posts, setPosts] = useState([]);
+  const [content, setContent] = useState('');
+  const [email, setEmail] = useState('');
+  const [session, setSession] = useState(null);
+
+  useEffect(() => {
+    fetchPosts();
+  }, []);
+
+  useEffect(() => {
+    setSession(supabase.auth.session())
+
+    supabase.auth.onAuthStateChange((_event, session) => {
+      setSession(session)
+    })
+  }, []);
+
+
+  const handleLogin = async () => {
+    await supabase.auth.signIn({ email, password: "random" });
+  };
+
+  const handleLogout = async () => {
+    await supabase.auth.signOut();
+  };
+
+
+  async function fetchPosts() {
+    let { data: posts, error} = await supabase
+    .from("posts")
+    .select("*");
+
+    if (error) {
+      throw error;
+    }
+
+    setPosts(posts);
+  }
+
+
+  function handleContent(e) {
+    setContent(e.target.value);
+  }
+
+  function handleEmail(e) {
+    setEmail(e.target.value);
+  }
+
+  async function handleSubmit() {
+    //post content to supabase
+    await supabase.from("posts").insert({ content, user_id: session.user.id });
+
+
+    setContent("");
+    fetchPosts();
+  }
+
   return (
-    <div>
-      Hello World!
+    <div style={{margin: "2rem"}}>
+      <div>
+        {session ? (
+          <>
+          <p>Hello, {session.user.email}</p>
+          <button onClick={handleLogout}>Logout</button>
+          </>
+        ) : (
+          <>
+          <input type="email" value={email} onChange={handleEmail} />
+          <button onClick={handleLogin}>Login</button>
+          </>
+        )}
+      </div>
+      <h1>Posts by users</h1>
+      
+      {session &&
+      <div>
+        <input type="text" value={content} onChange={handleContent} />
+        <button onClick={handleSubmit}>Add Post</button>
+      </div>
+      }
+      
+      <div>
+      {posts.map((post) => (
+        <div key={post.id}>{post.content}</div>
+      ))}
+      </div>
     </div>
   );
 }
